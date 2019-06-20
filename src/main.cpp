89ecb5ee0526e34b8e502f9bcb54df6435589c59
Guilherme Bazzo <bazzoguilherme@gmail.com>
2019-06-19 23:40:54 -0300

    remove print

--- src/main.cpp
+++ src/main.cpp
@@ -472,7 +472,7 @@ int main(int argc, char* argv[])
             camera_lookat_l    = glm::vec4(posicoes_estandes[estande_atual].x, posicoes_estandes[estande_atual].y + 3.2f, posicoes_estandes[estande_atual].z, 1.0f);
             // Vetor "view", sentido para onde a câmera está virada
             camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
-            printf("x = %f, y = %f, z = %f\n", posicoes_estandes[estande_atual].x,posicoes_estandes[estande_atual].y,posicoes_estandes[estande_atual].z );
+            //printf("x = %f, y = %f, z = %f\n", posicoes_estandes[estande_atual].x,posicoes_estandes[estande_atual].y,posicoes_estandes[estande_atual].z );
         }
 
         camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
